edb0436c09669e69be758a7b06faf5b026ed4bac
JaloliddinIkrom <jaloliddinikromovvv@gmail.com>
2022-11-13 19:48:58 +0300

    Initialiy Commit

---
+++ js/carusel.js
@@ -0,0 +1,217 @@
+  
+
+  $('.contactis-carusel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    autoplaySpeed: 0,
+    arrows: false,
+    cssEase: 'linear',
+    speed: 5000,
+    initialSlide: 1,
+    draggable: false, 
+    responsive: [
+        {
+            breakpoint: 1209,
+            settings: {
+                slidesToShow: 3, 
+                slidesToScroll: 1,
+                infinite: true,                                       
+                
+            }
+        },
+        {
+            breakpoint: 909,
+            settings: {
+                slidesToShow: 2, 
+                slidesToScroll: 1,
+              
+               
+            }
+        }, 
+        {
+            breakpoint: 625,
+            settings: {
+                slidesToShow: 1, 
+                slidesToScroll: 0.5,
+          
+               
+            }
+        }, 
+         {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1, 
+              slidesToScroll: 0.5,
+           
+          
+          }
+      }
+    ]
+   
+  }); 
+$('.aside__carusel').slick({ 
+ 
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  autoplaySpeed: 0,
+  arrows: false,
+  cssEase: 'linear',
+  speed: 5000,
+  initialSlide: 1,
+  draggable: false, 
+  responsive: [
+    {
+        breakpoint: 1234,
+        settings: {
+            slidesToShow: 2, 
+            slidesToScroll: 1,
+            infinite: true,                                       
+            
+        }
+    },
+    {
+        breakpoint: 890,
+        settings: {
+            slidesToShow: 2, 
+            slidesToScroll: 1,
+          
+           
+        }
+    }, 
+    {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1, 
+            slidesToScroll: 0.5,
+      
+           
+        }
+    }, 
+     {
+      breakpoint: 480,
+      settings: {
+          slidesToShow: 1, 
+          slidesToScroll: 0.5,
+       
+      
+      }
+  }
+]
+   
+  }); 
+
+
+$('.carusel-cart').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    autoplaySpeed: 0,
+    arrows: false,
+    cssEase: 'linear',
+    speed: 5000,
+    initialSlide: 1,
+    draggable: false,  
+    responsive: [
+        {
+            breakpoint: 1234,
+            settings: {
+                slidesToShow: 2, 
+                slidesToScroll: 1,
+                infinite: true,                                       
+                
+            }
+        },
+        {
+            breakpoint: 890,
+            settings: {
+                slidesToShow: 2, 
+                slidesToScroll: 1,
+              
+               
+            }
+        }, 
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1, 
+                slidesToScroll: 0.5,
+          
+               
+            }
+        }, 
+         {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1, 
+              slidesToScroll: 0.5,
+           
+          
+          }
+      }
+    ]
+    
+  });  
+
+ 
+
+  $('.carusel-cart-activ').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    autoplaySpeed: 0,
+    arrows: false,
+    cssEase: 'linear',
+    speed: 5000,
+    initialSlide: 1,
+    draggable: false,
+    rtl: true, 
+    responsive: [
+        {
+            breakpoint: 1234,
+            settings: {
+                slidesToShow: 2, 
+                slidesToScroll: 1,
+                infinite: true,                                       
+                
+            }
+        },
+        {
+            breakpoint: 890,
+            settings: {
+                slidesToShow: 2, 
+                slidesToScroll: 1,
+              
+               
+            }
+        }, 
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1, 
+                slidesToScroll: 0.5,
+          
+               
+            }
+        }, 
+         {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1, 
+              slidesToScroll: 0.5,
+           
+          
+          }
+      }
+    ]
+  }); 
+ 
